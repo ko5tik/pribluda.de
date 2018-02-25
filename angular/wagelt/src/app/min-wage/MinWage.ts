27b1d2dd7122ae3e47593778dd9f70706f93c718
ko5tik <konstantin.pribluda@gmail.com>
2018-02-25 20:11:14 +0200

    reworked for minimal non-taxable income

--- angular/wagelt/src/app/min-wage/MinWage.ts
+++ angular/wagelt/src/app/min-wage/MinWage.ts
@@ -18,7 +18,7 @@ export class MinWage {
   calulateFromBrutto(number: number) {
     console.log('called calc form brutto: ' + number);
     this.wageBrutto = this.roundMoney(number);
-    this.wageNetto = this.wageBrutto - this.sodraAmountWorker - this.taxAmountWorker;
+    this.wageNetto = this.roundMoney(this.wageBrutto - this.sodraAmountWorker - this.taxAmountWorker);
     this.companyTotal = this.wageBrutto + this.sodraAmountCompany;
   }
 
@@ -28,9 +28,30 @@ export class MinWage {
    */
   calulateFromNetto(number: number) {
     console.log('calc form netto: ' + number);
-    //  netto % is 100 - tax - sodra worker
+    //  netto % is 100 - tax - sodra workercase
     this.wageNetto = this.roundMoney(number);
-    this.wageBrutto = this.roundMoney(this.wageNetto / (1 - this.sodraRateWorker - this.taxRateWorker));
+
+    // take into account non taxable minimum
+    // this is non taxable amount with SODRA9% taken out
+    if (this.wageNetto < 345.8) {
+      // no taxes in this, bus sodra still taken off this
+      this.wageBrutto = this.roundMoney(this.wageNetto / (1 - this.sodraRateWorker));
+      console.log("under taxable minimum, brutto:" + this.wageBrutto);
+    } else if(this.wageNetto < 361) {
+      //  we have  still 380 eur non taxable minimum,  so calc formula is
+      //  b = ( n - 57 ) / 0.76
+      this.wageBrutto = this.roundMoney((this.wageNetto -57) / 0.76);
+      console.log("max taxable minimum, brutto:" + this.wageBrutto);
+    } else if (this.wageNetto >= 881.6) {
+      // cut out for non taxable amount if 1160 bruto, corresponds to 881.6 netto
+      // non taxable minimum no longer apply
+      this.wageBrutto = this.roundMoney(this.wageNetto / (1 - this.sodraRateWorker - this.taxRateWorker));
+      console.log("non taxable not applies, brutto:" + this.wageBrutto);
+    } else {
+      // after much calculation I came to the formula b= (n-01.5*580)/ 0.685
+      this.wageBrutto = this.roundMoney((this.wageNetto - 87) / 0.685);
+      console.log("non taxable minimum applies , brutto:" + this.wageBrutto);
+    }
     this.companyTotal = this.roundMoney(this.wageBrutto + this.sodraAmountCompany);
   }
 
@@ -55,10 +76,34 @@ export class MinWage {
   }
 
   get taxAmountWorker(): number {
-    return this.roundMoney(this.wageBrutto * this.taxRateWorker);
+    var n = this.npd;
+
+    // we are below minimum,  nothing to pay here
+    if (this.wageBrutto < n) {
+      return 0;
+    }
+    // otherwise we pull out NPD,  when we have too much momey it goes against 0
+    return this.roundMoney((this.wageBrutto - n) * this.taxRateWorker);
   }
 
   get percentState(): number {
-    return this.roundMoney( this.wageNetto / this.companyTotal);
+    return this.roundMoney(this.wageNetto / this.companyTotal);
+  }
+
+  /**
+   * non taxable minimum -  380 below 400,   sinks to 0 at 1160
+   * @returns {number}
+   */
+
+  get npd(): number {
+    if (this.wageBrutto < 400) {
+      return 380;
+    }
+    var nn = 380 - 0.5 * (this.wageBrutto - 400);
+    return nn > 0 ? nn : 0;
+  }
+
+  get taxableAmount(): number {
+    return this.wageBrutto < 380 ? 0 : this.wageBrutto - this.npd;
   }
 }
